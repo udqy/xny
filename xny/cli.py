f1be03f191cdf12278836e5ab3bc3b64bf5b355b
udqy <udaygitlog@gmail.com>
2024-12-23 15:22:22 +0530

    feat: implement searching matches using fzf

--- xny/cli.py
+++ xny/cli.py
@@ -1,15 +1,19 @@
 import argparse
 from cache import update_cache
+from fetch import fetch
 
 def main():
     parser = argparse.ArgumentParser(description="Learn X in Y Minutes CLI")
     parser.add_argument("--update", action="store_true", help="Update cache")
+    parser.add_argument("topic", type=str, help="Name of the Language or Resource")
 
     args, unknown_args = parser.parse_known_args()
 
     if unknown_args:
         print("Unrecognized arguments:\n" + "\n".join(f" {arg}" for arg in unknown_args) + "\n")
         parser.print_help()
+    elif args.topic:
+        fetch(args.topic)
     elif args.update:
         update_cache()
     else:
---
+++ xny/fetch.py
@@ -0,0 +1,50 @@
+import re
+from cache import CACHE_DIR
+from pathlib import Path
+from difflib import SequenceMatcher
+from utils import fzf
+
+def search(topic):
+    normalized_topic = re.sub(r'[^a-zA-Z0-9]', '', topic.lower())
+    matches = []
+    perfect_matches = []
+    
+    for file_path in Path(CACHE_DIR).glob('*.md'):
+        filename = file_path.stem
+        normalized_filename = re.sub(r'[^a-zA-Z0-9]', '', filename.lower())
+        ratio = SequenceMatcher(None, normalized_topic, normalized_filename).ratio()
+        
+        if ratio == 1.0:
+            perfect_matches.append((str(file_path), ratio))
+        elif ratio > 0.6:
+            matches.append((str(file_path), ratio))
+    
+    # If we have perfect matches, return only those
+    if perfect_matches:
+        perfect_matches.sort(key=lambda x: x[1], reverse=True)
+        return perfect_matches
+    
+    # Otherwise return all matches sorted by ratio
+    matches.sort(key=lambda x: x[1], reverse=True)
+
+    # returns all the files if no matches are found
+    if len(matches)==0:
+        return [(str(f), 1.0) for f in Path(CACHE_DIR).glob('*.md')]
+    
+    return matches
+
+def fetch(topic):
+    print(f"|--> Searching for: {topic}")
+    matches = search(topic)
+    
+    if len(matches) == 1:
+        # match found
+        print(f"|--> Found {topic}")
+        # display logic
+    elif len(matches)<3:
+        # open fzf with only avl matches
+        fzf(matches)
+        # display logic
+    else:
+        print("|--X Couldn't find the specified resource -- try searching with fzf")
+        fzf(matches)
